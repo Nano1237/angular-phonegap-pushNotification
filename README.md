52ccb274d1e39c0ee0a89c198556dd909e5dbc4c
Tim <tim.ruecker@ymail.com>
2014-12-09 10:35:37 +0100

    init

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-angular-phonegap-pushNotification
-=================================
+# angular-phonegap-pushNotification
 
-AngularJS Module for the PushPlugin
+AngularJS Module for the [PushPlugin](https://github.com/phonegap-build/PushPlugin/blob/1979d972b6ab37e28cf2077bc7ebfe706cc4dacd/README.md)
+
+#####Dependencies
+- It's build for the [phonegap-build/PushPlugin](https://github.com/phonegap-build/PushPlugin/blob/1979d972b6ab37e28cf2077bc7ebfe706cc4dacd/README.md)
+- The Phonegap [Device Plugin](https://github.com/apache/cordova-plugin-device/blob/c6e23d8a61793c263443794d66d40723b4d04377/doc/index.md)
+- [AngularJS](https://angularjs.org/) (*Who would have thought it?*)
+
+##Usage
+
+- Install the `Cordova Push Notifications Plugin`
+- Install the `Device Plugin`
+- Install the `angular-phonegap-pushNotification Plugin`
+- Add the `angular-phonegap-pushNotification Plugin` to the Apps Dependencies:
+```
+angular.module('app', ['angular-phonegap-pushNotification']);
+```
+- Set the `registered` Event **BEFORE** the `pushNotification.init` (*see next point*)
+```
+ pushNotification.addListener('registered',function(e){
+    e.regid;//The RegistrationId for the Device (needed to send Push Notifications)
+ });
+```
+- Initalize the Plugin with the `pushNotification.init` Method **AFTER** the Cordovas `deviceready` Event
+```
+...
+document.addEventListener('deviceready', function() {
+    pushNotification.init('0000000000');//The google senderID
+});
+...
+```
+- Now add Listeners for (*for example*) incoming Messages (you can add this Listener Anytime, its not neccesary that you add it after the two pints before)
+```
+pushNotification.addListener('message',function(e){
+    e;//The Message Object
+ });
+```
---
+++ angular-phonegap-pushNotification.js
@@ -0,0 +1,163 @@
+(function(window) {
+    window.angular.module('angular-phonegap-pushNotification', [])
+
+            /**
+             * 
+             * @description The default device registration if the "device" plugin isnt installed
+             * @returns {_L1._L11.Anonym$2|_L1._L16.Anonym$2}
+             */
+            .constant('angular-phonegap-pushNotification.defaultDevice', 'android')
+
+            /**
+             * 
+             * @description The pluginmethods as factory object
+             * @returns {_L1._L11.Anonym$2}
+             */
+            .factory('angular-phonegap-pushNotification.plugin', [
+                function() {
+                    var cordova = window.cordova;
+                    return {
+                        /**
+                         * 
+                         * @description Registers a Device for the PushNotifications
+                         * @param {Object} options The registration options
+                         * @param {[Function,Undefined]} successCallback
+                         * @param {[Function,Undefined]} errorCallback
+                         * @returns {undefined}
+                         */
+                        register: function(options, successCallback, errorCallback) {
+                            successCallback = successCallback || function() {
+                            };
+                            errorCallback = errorCallback || function() {
+                            };
+                            cordova.exec(successCallback, errorCallback, "PushPlugin", "register", [options]);
+                        },
+                        /**
+                         * 
+                         * @description Unregisters a Device from the PushNotifications
+                         * @param {[Function,Undefined]} successCallback
+                         * @param {[Function,Undefined]} errorCallback
+                         * @returns {undefined}
+                         */
+                        unregister: function(successCallback, errorCallback) {
+                            successCallback = successCallback || function() {
+                            };
+                            errorCallback = errorCallback || function() {
+                            };
+                            cordova.exec(successCallback, errorCallback, "PushPlugin", "unregister", []);
+                        },
+                        /**
+                         * 
+                         * @description Sets the Apple Badgenumber
+                         * @param {type} badge The Badge informations
+                         * @param {[Function,Undefined]} successCallback
+                         * @param {[Function,Undefined]} errorCallback
+                         * @returns {undefined}
+                         */
+                        setApplicationIconBadgeNumber: function(badge, successCallback, errorCallback) {
+                            successCallback = successCallback || function() {
+                            };
+                            errorCallback = errorCallback || function() {
+                            };
+                            cordova.exec(successCallback, successCallback, "PushPlugin", "setApplicationIconBadgeNumber", [{badge: badge}]);
+                        }
+                    };
+                }
+            ])
+
+            /**
+             * 
+             * @description Pushmethods combined as factory
+             * @param {type} plugin The Plugin Methods
+             * @param {type} defaultDevice The Defualt device (android)
+             * @returns {_L1._L51.ret}
+             */
+            .factory('pushNotification', [
+                'angular-phonegap-pushNotification.plugin',
+                'angular-phonegap-pushNotification.defaultDevice',
+                function(plugin, defaultDevice) {
+                    var device = window.device;
+                    var cordova = window.cordova || false;//checkvariable for cordova Plugin
+
+                    /**
+                     * 
+                     * @description Contains all registered EventListeners
+                     * @type Array
+                     */
+                    var registeredFunctions = [];
+                    return {
+                        /**
+                         * 
+                         * @description Manualy calls all eventlisteners of a type
+                         * @param {String} type The type of the calling listeners
+                         * @param {Variable} params param for the method
+                         * @returns {undefined}
+                         */
+                        callListener: function(type, params) {
+                            for (var i in registeredFunctions) {
+                                if (typeof registeredFunctions[i] !== 'undefined' && typeof registeredFunctions[i].type !== 'undefined') {
+                                    if (registeredFunctions[i].type === type) {
+                                        registeredFunctions[i].method(params);
+                                    }
+                                }
+                            }
+                        },
+                        /**
+                         * 
+                         * @description Initializes the plugin with the google senderId
+                         * It has top be called after "deviceready"!
+                         * @param {[String,Number]} appid The google SenderId
+                         * @returns {undefined}
+                         */
+                        init: function(appid) {
+                            if (!cordova) {
+                                alert('Cordova Plugin not found!');
+                                return;
+                            }
+                            var self = this;
+                            window.angularphonegappushNotificationonNotification = function(e) {
+                                self.callListener(e.event, e);
+                            };
+                            var platform = typeof device !== 'undefined' ? device.platform || defaultDevice : defaultDevice;
+                            //
+                            //
+                            if (platform === 'android' || platform === 'Android') {
+                                plugin.register({
+                                    "senderID": appid || '',
+                                    "ecb": 'angularphonegappushNotificationonNotification'
+                                });
+                            } else {//iphone currently not ready!!
+                                plugin.register({
+                                    "badge": "true",
+                                    "sound": "true",
+                                    "alert": "true",
+                                    "ecb": "onNotificationAPN"
+                                });
+                            }
+                        },
+                        /**
+                         * 
+                         * @description Adds a new eventlistener for a certain event type
+                         * @param {type} type The type of the listener (can also be a custom listener)
+                         * @param {type} method The calling method
+                         * @returns {registeredFunctions.lenght} The index of the current event for delete
+                         */
+                        addListener: function(type, method) {
+                            var newEvent = {type: type, method: method};
+                            var length = registeredFunctions.lenght;
+                            registeredFunctions.push(newEvent);
+                            return length;
+                        },
+                        /**
+                         * 
+                         * @description Removes a event from the registeredFunctions array
+                         * @param {Number} index The index of the deleting event
+                         * @returns {Boolean}
+                         */
+                        removeListener: function(index) {
+                            return delete registeredFunctions[index];
+                        }
+                    };
+                }
+            ]);
+})(window);
